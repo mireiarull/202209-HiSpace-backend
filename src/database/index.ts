9220b66e98aba51e2c12d9455165fddb99b8ee74
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-08 21:03:10 +0100

    Add mongoose setting in database index

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,6 +2,7 @@ import "../loadEnvirontment.js";
 import mongoose from "mongoose";
 import debugCreator from "debug";
 import chalk from "chalk";
+import environment from "../loadEnvirontment.js";
 
 const debug = debugCreator("robots: database");
 
@@ -9,6 +10,18 @@ const connectDatabase = async (mongoUrl: string) => {
   try {
     await mongoose.connect(mongoUrl);
     debug(chalk.green("Connection to database was successfull"));
+
+    mongoose.set("debug", environment.mongoDbDebug === "true");
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform(doc, ret) {
+        delete ret._id;
+        delete ret.__v;
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return ret;
+      },
+    });
   } catch (error: unknown) {
     debug("Error on connecting to database", (error as Error).message);
   }
