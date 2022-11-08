35ee321c9e4cb96dc5a79de46a1a56161849b606
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-08 20:46:37 +0100

    Add auth to robotsRouter middleware chain

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,6 +4,7 @@ import { generalError, notFoundError } from "./middlewares/error.js";
 import robotsRouter from "./routers/robotsRouter.js";
 import cors from "cors";
 import userRouter from "./routers/usersRouter.js";
+import auth from "./middlewares/auth.js";
 
 const app = express();
 
@@ -16,7 +17,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/users", userRouter);
-app.use("/robots", robotsRouter);
+app.use("/robots", auth, robotsRouter);
 
 app.use(notFoundError);
 app.use(generalError);
