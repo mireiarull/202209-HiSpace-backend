6283269e177247b76114dd2da72e4e5a16d2ce18
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-08 20:19:38 +0100

    Add login user controller

--- src/server/controllers/userControllers.ts
+++ src/server/controllers/userControllers.ts
@@ -1,13 +1,45 @@
 import "../../loadEnvirontment.js";
-import type { Request, Response } from "express";
-
+import type { NextFunction, Request, Response } from "express";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import type { Credentials, UserTokenPayload } from "../../types.js";
+import User from "../../database/models/User.js";
+import CustomError from "../../CustomError/CustomError.js";
+import environment from "../../loadEnvirontment.js";
 
-const loginUser = (req: Request, res: Response) => {
-  const secret = process.env.JWT_SECRET;
-  const token = jwt.sign({}, secret);
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { username, password } = req.body as Credentials;
 
-  res.status(201).json({ token });
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    const error = new CustomError(
+      "Username not found",
+      401,
+      "Wrong credentials"
+    );
+    next(error);
+    return;
+  }
+
+  if (!(await bcrypt.compare(password, user.password))) {
+    const error = new CustomError(
+      "Password is incorrect",
+      401,
+      "Wrong credentials"
+    );
+    next(error);
+    return;
+  }
+
+  const tokenPaylod: UserTokenPayload = {
+    id: user._id.toString(),
+    username,
+  };
+
+  const token = jwt.sign(tokenPaylod, environment.jwtSecret);
+
+  res.status(200).json({ accessToken: token });
 };
 
 export default loginUser;
--- src/server/middlewares/error.test.ts
+++ src/server/middlewares/error.test.ts
@@ -1,32 +1,9 @@
 import type { Response } from "express";
 import CustomError from "../../CustomError/CustomError";
-import { notFoundError, generalError } from "./error";
+import { generalError } from "./error";
 
 beforeEach(() => {
   jest.clearAllMocks();
-});
-
-describe("Given a notFoundError middleware", () => {
-  describe("When it receives a response", () => {
-    const res: Partial<Response> = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn().mockReturnThis(),
-    };
-
-    test("Then it should call its method status with a 404", () => {
-      const statusCode = 404;
-      notFoundError(null, res as Response, () => {});
-
-      expect(res.status).toHaveBeenCalledWith(statusCode);
-    });
-
-    test("Then it should call its method json with 'unknown enpoint'", () => {
-      const expectedErrorMessage = { message: "unknown enpoint" };
-      notFoundError(null, res as Response, () => {});
-
-      expect(res.json).toHaveBeenCalledWith(expectedErrorMessage);
-    });
-  });
 });
 
 describe("Given a generalError middleware", () => {
