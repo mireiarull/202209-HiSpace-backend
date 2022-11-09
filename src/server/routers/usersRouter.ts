6421f2ed7c83a89c95e6515e03c310d398e9b1a8
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-09 07:42:00 +0100

    Remove options middlwares from robots and users

--- src/server/routers/usersRouter.ts
+++ src/server/routers/usersRouter.ts
@@ -1,15 +1,9 @@
 import "../../loadEnvirontment.js";
 import express from "express";
 import { loginUser, registerUser } from "../controllers/userControllers.js";
-import app from "../app.js";
 
 // eslint-disable-next-line new-cap
 const userRouter = express.Router();
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
 
 userRouter.post("/login", loginUser);
 userRouter.post("/register", registerUser);
